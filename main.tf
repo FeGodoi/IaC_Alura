188df2713ccdcf8c57c3abcd6075df1f77b80a4d
Felipe Godoi <flg.fca@gmail.com>
2023-05-20 23:42:02 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.27"
+      }
+    }
+    
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+    profile = "default"
+    region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+    ami = "ami-0fcf52bcf5db7b003"
+    instance_type = "t2.micro"
+    key_name = "iac_alura"
+    tags = {
+      Name = "Primeira instancia"
+    }
+}
